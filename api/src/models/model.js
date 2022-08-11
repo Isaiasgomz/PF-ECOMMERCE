9543d06d3c9e5c618a93f74aed9599842d15e76b
Isaiasgomz <isaiasgomz027@gmail.com>
2022-08-11 11:42:18 -0500

    fix

--- api/src/models/model.js
+++ api/src/models/model.js
@@ -3,7 +3,7 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('country', {
+  sequelize.define('main', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
