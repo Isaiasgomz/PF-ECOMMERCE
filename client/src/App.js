6bb1bb72bbaa5c84f5e93a3636a5eeb81ed7d60f
Bautista Baigorri <bautibaigorri.bb@gmail.com>
2022-08-12 16:10:11 -0300

    landing

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar"
 import Home from "./components/Home/Home"
+import Landing from './components/Landing/Landing';
 
 
 
@@ -10,11 +11,9 @@ function App() {
 
   return (
     <div className="App">
-     
+      <Route exact path="/" component={Landing} />
       <Route path="/" component={NavBar} />
       <Route exact path="/home" component={Home} />
-
-
     </div>
   );
 }
