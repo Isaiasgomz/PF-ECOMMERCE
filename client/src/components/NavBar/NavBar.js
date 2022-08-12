7985a4e445fb74c361ef7f05a8ea6c87c074fc0f
leotano <leonelfrobledo@gmail.com>
2022-08-12 13:46:46 -0300

    first push

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+
+const [search, setSearch] = useState({name:""})
+
+const onChange = (e) => {
+  setSearch({
+      ...search,
+      [e.target.name]: e.target.value
+  })
+}
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+
+    <div>
+      NavBar
+      <form>
+      <input type="text" name="name" value={search.name} onChange={onChange}></input>
+      <button type='submit'>search</button>
+      </form>
+
+      </div>
   )
 }
 
