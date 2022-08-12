336c15cee02b0aa4a3deeec03a4d8fd635b81031
agustinslens <aglens18@gmail.com>
2022-08-12 10:22:32 -0300

    db product

---
+++ api/src/models/Product.js
@@ -0,0 +1,49 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+    // defino el modelo
+    sequelize.define('product', {
+        id:{
+            type:DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        productName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }, 
+        price: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+        }, 
+        image: {
+            type: DataTypes.STRING, 
+            allowNull: false,
+        }, 
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        }, 
+        qualification: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate:{
+                max:5,
+                min:1
+            }
+        }, 
+        reviews: {
+            type: DataTypes.TEXT,
+        }, 
+        quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue:0
+        }, 
+        category: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    });
+};
