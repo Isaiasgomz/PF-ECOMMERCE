1888f78a2d78d4aefba9378598380dc2b2a5812b
Isaiasgomz <isaiasgomz027@gmail.com>
2022-08-12 11:35:23 -0500

    Route Seller

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -25,6 +25,15 @@ module.exports = (sequelize) => {
             type: DataTypes.TEXT,
             allowNull: false,
         }, 
+        category: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue:0
+        }, 
         qualification: {
             type: DataTypes.INTEGER,
             allowNull: false,
@@ -36,14 +45,5 @@ module.exports = (sequelize) => {
         reviews: {
             type: DataTypes.TEXT,
         }, 
-        quantity: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            defaultValue:0
-        }, 
-        category: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        }
     });
 };
