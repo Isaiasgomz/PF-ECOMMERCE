714b3c3665c5b8d7860d700bb0196e0f20e2fc7e
Marquisl2 <marcoslaurens25@gmail.com>
2022-08-12 16:38:01 -0300

    add component footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,7 +1,25 @@
 import React from "react";
+import style from "./Footer.module.css"
 
 function Footer() {
-  return <div>Footer</div>;
+  return(
+    <div className={style.containerFooter}>
+      <div className={style.containerCopy}>
+        <p className={style.copyright}>Copyright © 2022 TechStore S.R.L</p>
+      </div>
+      <div className={style.containerButtons}>
+        <button className={style.buttonFooter}>FAQ's</button>
+        <button className={style.buttonFooter}>Help</button>
+        <button className={style.buttonFooter}>About</button>
+      </div>
+      <div className={style.containerIcons}>
+      <i class="fa-brands fa-instagram"></i>
+      <i class="fa-brands fa-facebook-f"></i>
+      <i class="fa-brands fa-linkedin-in"></i>
+      </div>
+    </div>
+    ) ;
+    
 }
 
 export default Footer;
