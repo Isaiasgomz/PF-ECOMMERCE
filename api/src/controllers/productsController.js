d38c61d48a53d67708a6337804ce2852e5c98500
agustinslens <aglens18@gmail.com>
2022-08-13 10:24:41 -0300

    modif get products

--- api/src/controllers/productsController.js
+++ api/src/controllers/productsController.js
@@ -1,48 +1,49 @@
-const {Op, Product} =require('../db');
+const { Op, Product } = require('../db');
 const api = require("../jsonProducts.js");
 
-module.exports ={
+module.exports = {
     /* producto por query */
-    productByName: async function (name){
-        let product = await Product.findAll()
-        if (product.length === 0) {
-            throw 'el producto no existe';
-        } else {
-            product = product.filter(e=>e.productName.toLowerCase().includes(name.toLowerCase()))
+    productByName: async function (name) {
+        try {
+            let product = await Product.findAll()
+            product = product.filter(e => e.productName.toLowerCase().includes(name.toLowerCase()))
+            if (product.length === 0) throw 'el producto no existe';
             return product;
+        } catch (error) {
+            throw error;
         }
     },
     /* meter todos los productos desde la api a la base de datos */
-    listProducts: async function(){
+    listProducts: async function () {
         const allProducts = await Product.findAll()
-        if(allProducts.length===0){
+        if (allProducts.length === 0) {
             let obj = {}
             let filtrado = api.map(e => {
                 obj = {
-                    productName:e.name,
-                    price:e.price,
-                    image:e.image,
-                    brand:e.brand,
-                    description:e.description,
-                    qualification:e.calification,
-                    stock:e.quantity,
-                    category:e.categories
+                    productName: e.name,
+                    price: e.price,
+                    image: e.image,
+                    brand: e.brand,
+                    description: e.description,
+                    qualification: e.calification,
+                    stock: e.quantity,
+                    category: e.categories
                 }
                 return obj;
             })
             await Product.bulkCreate(filtrado);
             return filtrado;
-        }else{
+        } else {
             return allProducts;
         }
     },
     /* detalle de producto por params */
-    productDetail: async function(idProduct){
-        let product = await Product.findByPk(idProduct)
-        if (!product) {
-            throw 'el id del producto solicitado no existe';
-        } else {
+    productDetail: async function (idProduct) {
+        try {
+            let product = await Product.findByPk(idProduct)
             return product;
+        } catch (error) {
+            throw 'el id del producto solicitado no existe';
         }
     }
 }
--- api/src/routes/Products.js
+++ api/src/routes/Products.js
@@ -1,24 +1,11 @@
 const { Router } = require('express');
 const router = Router();
-const axios = require('axios');
 const { productByName, listProducts, productDetail } = require('../controllers/productsController');
-const jsonProducts = require('/api/src/jsonProducts')
 
-const { Product} = require('../db')
-
- async function  apiInfo (){
-    await Product.bulkCreate(jsonProducts)
-}
 
 router.get('/', async (req, res) => {
     const { name } = req.query;
     try {
-
-    const verification= await Product.count()
-     if(verification < 1){
-        await apiInfo()
-    }
-
     if(name){
             let productName = await productByName(name);
             res.status(200).send(productName);
@@ -33,14 +20,10 @@ router.get('/', async (req, res) => {
 router.get('/:idProduct', async (req, res) => {
     const { idProduct } = req.params;
     try {
-        const verification= await Product.count()
-        if(verification < 1){
-            await apiInfo()
-        }
-
         let prodDetail = await productDetail(idProduct);
         res.status(200).send(prodDetail);
     } catch (error) {
+        console.log(error)
         res.status(404).send(error);
     }
 })
