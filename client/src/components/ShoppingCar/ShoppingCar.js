7e5889390e0f8d811e66ca2d436e607677ae531d
Marquisl2 <marcoslaurens25@gmail.com>
2022-08-24 18:24:46 -0300

    add function clearAllCart

--- client/src/components/ShoppingCar/ShoppingCar.js
+++ client/src/components/ShoppingCar/ShoppingCar.js
@@ -65,15 +65,30 @@ function ShoppingCar() {
 
   const handleClickNoVerified = (e)=>{
     e.preventDefault()
+    
+    if(user.email_verified === false){
+      swal("No estás verificado!", "Verificación enviada a su casilla de correo", "error");
+      return
+    }
+
+    
+    if(!user){
+      swal("No estas logeado!", "Para realizar una compra debes de estar logeado", "error");
+      return
+    }
+
     if(!productsFromLocalStorage.length){
       swal("Carrito vacío!", "No hay productos en el carrito", "error");
       return
     }
-    if(user){
-      swal("No estás verificado!", "Verificación enviada a su casilla de correo", "error");
-    }else{
-      swal("No estas logeado!", "Para realizar una compra debes de estar logeado", "error");
-    }
+    
+  }
+
+  const clearCart = (e)=>{
+    e.preventDefault()
+    localStorage.removeItem(stringLocalStorage)
+    localStorage.setItem(stringLocalStorage,JSON.stringify([]))
+    setPrice(0)
   }
  
 
@@ -119,6 +134,11 @@ function ShoppingCar() {
           <h2>No hay productos!</h2>
         )}
       </div>
+      {
+        productsFromLocalStorage.length &&
+        Array.isArray(productsFromLocalStorage)?
+        <div className={style.containerClearCart}><button className={style.button} onClick={e=>clearCart(e)}>Vaciar carrito</button></div> : ""
+      }
     </div>
   );
 }
