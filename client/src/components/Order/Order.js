bc7b7592be753193e6f73ae0f541e7726ba897ae
Bautista Baigorri <bautibaigorri.bb@gmail.com>
2022-08-26 13:22:16 -0300

    fix order

--- client/src/components/Order/Order.js
+++ client/src/components/Order/Order.js
@@ -126,7 +126,7 @@ function Order() {
             type="text"
             name="address"
             value={input.address}
-            placeholder="Calle, número, localidad, departamento. País"
+            placeholder="Calle y número"
             onChange={(e) => handleInput(e)}
           />
         </p>
