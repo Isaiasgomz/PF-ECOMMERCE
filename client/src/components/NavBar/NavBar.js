ba1c88a30244a23058b2039e5320eb3532d704ad
Bautista Baigorri <bautibaigorri.bb@gmail.com>
2022-08-26 15:32:29 -0300

    fix navbar validate

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -7,7 +7,7 @@ import Menu from "../Menu/Menu";
 import LoginButton from "../auth0/LogginButton/ButtonLogin";
 import LogoutButton from "../auth0/logout/ButtonLogout";
 import Profile from "../auth0/User/User";
-import logo from "../../imagenes/logo.png"
+import logo from "../../imagenes/logo.png";
 
 function NavBar() {
   const { isAuthenticated } = useAuth0();
@@ -38,16 +38,16 @@ function NavBar() {
           )}
           {isAuthenticated ? (
             <>
-            <Link to="/userPanel">
-              <button className={styles.profile}>
-                <i className="fa-solid fa-circle-user"></i> Mi Perfil
-              </button>
+              <Link to="/userPanel">
+                <button className={styles.profile}>
+                  <i className="fa-solid fa-circle-user"></i> Mi Perfil
+                </button>
               </Link>
-            <Link to="/cart">
-              <button className={styles.cart}>
-                <i className="fa-solid fa-cart-shopping"></i> Carrito
-              </button>
-            </Link>
+              <Link to="/cart">
+                <button className={styles.cart}>
+                  <i className="fa-solid fa-cart-shopping"></i> Carrito
+                </button>
+              </Link>
             </>
           ) : (
             <Link to="/cart">
