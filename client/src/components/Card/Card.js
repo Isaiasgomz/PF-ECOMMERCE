da084c73581c607c45563a7e1f6ea746f00f2e60
leotano <leonelfrobledo@gmail.com>
2022-08-29 17:18:07 -0300

    etiqueta agotado

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -2,20 +2,35 @@ import React, { useState } from 'react'
 import style from "./Card.module.css"
 import { Rating } from "@mui/material";
 import { Link } from "react-router-dom";
+import agotado from "../../imagenes/agotado.png"
 import toast, { Toaster } from 'react-hot-toast';
 
-function Card({name,price,img,calification,localStor,ob,id,stock,notify}) {
-  
+function Card({ name, price, img, calification, localStor, ob, id, stock, notify }) {
+
 
   return (
     <div className={style.containerCard}>
-      <Link to={`/detail/${id}`}><div className={style.containerImg}>
-        <img className={style.img} src={img} alt={name} /> 
-      </div></Link>
+      {stock <= 0 ? <div className={style.containerAgotado}>
+        <div className={style.contAgotado}>
+          <img className={style.contAgotado} src={agotado} alt="agotado" />
+        </div>
+        <Link to={`/detail/${id}`}>
+          <div className={style.containerImgAgot}>
+            <img className={style.imgAgot} src={img} alt={name} />
+          </div>
+        </Link>
+      </div>
+        :
+        <Link to={`/detail/${id}`}>
+          <div className={style.containerImg}>
+            <img className={style.img} src={img} alt={name} />
+          </div>
+        </Link>}
+
       <div className={style.containerInfo}>
         <div className={style.containerTitle}>
 
-        <Link to={`/detail/${id}`}> <span>{name}</span></Link>
+          <Link to={`/detail/${id}`}> <span>{name}</span></Link>
         </div>
         <div className={style.containerPriceCart}>
           <span>${price}</span>
@@ -26,17 +41,17 @@ function Card({name,price,img,calification,localStor,ob,id,stock,notify}) {
             precision={0.5}
             readOnly
           /> */}
-          {stock<=0?
-          <div className={style.buttonCarrito}><i  className="fa-solid fa-cart-plus"></i></div>:
-          <div className={style.buttonCarrito} onClick={notify}><i onClick={()=>localStor(ob)} className="fa-solid fa-cart-plus"></i></div>}
+          {stock <= 0 ?
+            <div className={style.buttonCarritoAgotado}> <span> <i className="fa-solid fa-cart-plus"></i> </span> </div> :
+            <div className={style.buttonCarrito} onClick={notify}><i onClick={() => localStor(ob)} className="fa-solid fa-cart-plus"></i></div>}
           {/* <div>
       <button onClick={notify}>Make me a toast</button>
       <Toaster />
     </div> */}
-          
+
         </div>
       </div>
-      
+
     </div>
   )
 }
