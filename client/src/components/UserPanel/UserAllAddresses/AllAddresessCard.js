4b64e08667802e24eaf269d7728e1e62d5ae3074
agustinslens <aglens18@gmail.com>
2022-09-07 11:47:37 -0300

    fixes

--- client/src/components/UserPanel/UserAllAddresses/AllAddresessCard.js
+++ client/src/components/UserPanel/UserAllAddresses/AllAddresessCard.js
@@ -8,21 +8,17 @@ const AllAddresessCard = ({ reference, address, city, country, id }) => {
     <div className={style.card}>
       <div className={style.fila}>
         <div className={style.text}><i className="fa-solid fa-house"></i>  {reference}</div>
-        /*<div className={style.adress}>*/
+        
         <span className={style.spanText}>
           {address}, {city}, {country}
         </span>
         <Link to={`/updateAddres/${id}`}>
-          <div className={styl
-          e.containerPencil}>
+          <div className={style.containerPencil}>
             <i className="fa-solid fa-pencil"></i>
           </div>
-          {/* <button className={style.button}> Editar </button> */}
         </Link>
       </div>
-      {/* <div className={style.fila}>
-     
-      </div> */}
+    
     </div>
   );
 };
