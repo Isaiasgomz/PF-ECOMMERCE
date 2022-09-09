6952ea52e5c36d0b0fb5bbebbe136eb4421369b6
Marquisl2 <marcoslaurens25@gmail.com>
2022-09-09 13:11:25 -0300

    add loading in home

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -2,19 +2,32 @@ import React, { useEffect, useState } from "react";
 import Cards from "../Cards/Cards.js";
 import Filter from "../Filter/Filter.js";
 import styles from "./Home.module.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../../Actions/index.js";
 import ScrollToTop from "react-scroll-to-top";
+import loadingLogo from "../../imagenes/loading.png"
 
 function Home(props) {
   const dispatch = useDispatch();
 
   const [currentPage, setCurrentPage] = useState(1); //pagina actual
 
+  const { Products } = useSelector((state) => state);
+
   /*   useEffect(() => {
     dispatch(getProducts());
   }, [dispatch]); */
 
+  if(!Products.length){
+    return (
+      <div className={styles.contenedorLoading}>
+        <div className={styles.loading}>
+          <img className={styles.imgload} alt="loading" src={loadingLogo} />
+        </div>
+      </div>
+    )
+  }
+  else{
   return (
     <div className={styles.homeContainer}>
       <div className={styles.filterContainer}>
@@ -38,7 +51,7 @@ function Home(props) {
         }}
       />
     </div>
-  );
+  )};
 }
 
 export default Home;
